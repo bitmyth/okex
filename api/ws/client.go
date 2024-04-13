5b69a1ce26cd171afe43641a24f1a22a04cdb21b
bitmyth <bitmyth@outlook.com>
2024-04-13 09:58:12 +0800

    Update

--- api/ws/client.go
+++ api/ws/client.go
@@ -279,8 +279,8 @@ func (c *ClientWs) sender(p bool) error {
 				return err
 			}
 		case <-ticker.C:
-			c.mu[p].Lock() // prevent concurrent map read and write
 			if c.conn[p] != nil && (c.lastTransmit[p] == nil || (c.lastTransmit[p] != nil && time.Since(*c.lastTransmit[p]) > PingPeriod)) {
+				c.mu[p].Lock() // prevent concurrent map read and write
 				go func() {
 					c.sendChan[p] <- []byte("ping")
 					c.mu[p].Unlock()
